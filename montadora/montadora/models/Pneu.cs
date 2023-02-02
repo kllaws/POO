402694aec1e6b2de82a64abc5711c99cd0e279d1
Claudio Martins Da Silva <123429011+kllaws@users.noreply.github.com>
2023-02-02 19:26:44 -0300

    up

---
+++ montadora/montadora/models/Pneu.cs
@@ -0,0 +1,17 @@
+﻿namespace models
+{
+    public class Pneu
+    {
+        public int Aro { get; set; }
+        public bool TWI { get; set; }
+        public string Cor  { get; set; }
+        public string Marca { get; set; }
+        public int VelocidadeMaxima { get; set; }
+        public int VelocidadeAtual { get; set; }
+        public bool Estepe { get; set; }
+        public bool Estourado { get; set; }
+        public string Tipo { get; set; }
+
+
+    }
+}
