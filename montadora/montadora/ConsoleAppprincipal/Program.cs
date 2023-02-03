df8acb04c36ea34c68f6a76b6f9c135f2973f319
Claudio Martins Da Silva <123429011+kllaws@users.noreply.github.com>
2023-02-02 21:29:09 -0300

    vc

--- montadora/montadora/ConsoleAppprincipal/Program.cs
+++ montadora/montadora/ConsoleAppprincipal/Program.cs
@@ -1,10 +1,40 @@
-﻿namespace ConsoleAppprincipal
+﻿using models;
+
+namespace ConsoleAppprincipal
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-          
+            Pneu pneu1 = new Pneu();
+            pneu1.Cor = "preto";
+            pneu1.Tipo = "carro de passeio";
+            pneu1.Marca = "Firestone";
+            pneu1.Aro = 16;
+            pneu1.Furado = false;
+            pneu1.Estourado = false;
+            pneu1.Estepe = false;
+            pneu1.VelocidadeAtual = 0;
+            pneu1.VelocidadeMaxima = 150;
+            pneu1.PercentualBorracha = 100;
+            pneu1.TWI = false;
+            pneu1.Girar(5);
+
+            pneu1.Estourado = true;
+
+            Console.WriteLine("Cor: "+pneu1.Cor);
+            Console.WriteLine("Tipo: "+pneu1.Tipo);
+            Console.WriteLine("Marca: "+ pneu1.Marca);
+            Console.WriteLine("Aro: " + pneu1.Aro);
+            Console.WriteLine("Furado: "+pneu1.Furado);
+            Console.WriteLine("Estourado: "+pneu1 .Estourado);
+            Console.WriteLine("Estepe: "+pneu1.Estepe);
+            Console.WriteLine("velocidadeAtual: "+pneu1.VelocidadeAtual);
+            Console.WriteLine("VelocidadeMaxima: "+pneu1.VelocidadeMaxima); 
+            Console.WriteLine("TWI: "+pneu1.TWI);   
+
+
+
         }
     }
 }
--- montadora/montadora/models/Pneu.cs
+++ montadora/montadora/models/Pneu.cs
@@ -2,6 +2,8 @@
 {
     public class Pneu
     {
+        private int _reducao;
+
         public int Aro { get; set; }
         public bool TWI { get; set; }
         public string Cor  { get; set; }
@@ -11,7 +13,56 @@
         public bool Estepe { get; set; }
         public bool Estourado { get; set; }
         public string Tipo { get; set; }
+        public int PercentualBorracha { get; set; }
+        public bool Furado { get; set; }
 
+        public void Girar( int _velocidade)
+        {
+            if(Estourado)
+            {
+                Console.WriteLine("O pneu esta estourado");
+                return;
+            }
+            if(Furado)
+            {
+                Console.WriteLine("Pneu esta furado");
+                return ;
+            }
 
+            VelocidadeAtual = VelocidadeAtual + _velocidade;
+            PercentualBorracha -= -3;
+
+            if (VelocidadeAtual > VelocidadeMaxima || PercentualBorracha <=30)
+            {
+                EstourarPeneu();
+            }
+
+        }
+        public void Frear(int _reducao)
+        {
+            VelocidadeMaxima = VelocidadeMaxima - _reducao;
+            PercentualBorracha -= 5;
+            if(VelocidadeAtual < 0)
+            {
+                VelocidadeAtual = 0;
+            }
+
+            if (PercentualBorracha < 0)
+            {
+                PercentualBorracha = 0;
+            }
+
+            if(PercentualBorracha <= 30)
+            {
+                EstourarPeneu();
+            }
+
+        }
+
+        private void EstourarPeneu()
+        {
+            Estourado = true;
+            VelocidadeAtual = 0;
+        }
     }
 }
